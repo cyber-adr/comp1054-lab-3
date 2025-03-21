ca34dcea96cae4f969e745f159145e93588ef97c
cyber-adr <naimnz.7780@gmail.com>
2025-03-21 16:58:56 -0400

    js/lab3.js: JS completed

--- js/lab3.js
+++ js/lab3.js
@@ -1,22 +1,29 @@
 // Grab the List Elements and the Div elements separately
-const listElements = document.querySelectorAll("li");
+const anchorElements = document.querySelectorAll('li>a'); // targets anchor tags inside tabs
 
-const contentElementOne = document.querySelector("#tabpanel1");
-const contentElementTwo = document.querySelector("#tabpanel2");
-const contentElementThree = document.querySelector("#tabpanel3");
+const tabpanels = document.querySelectorAll('[role=\"tabpanel\"]'); // targets divs with role="tabpanel" attributes
+
+anchorElements.forEach(anchor => {
+    anchor.addEventListener('click', event => {
+        // https://www.geeksforgeeks.org/how-to-check-the-given-element-has-the-specified-class-in-javascript/
+        if (!anchor.classList.contains('open')) {
+            anchorElements.forEach(anchorCopy => {anchorCopy.classList.remove('open')}) // Remove class="open" for all list
+            anchor.classList.add('open'); // Then add the class="open" to the list of the current iteration of the outer loop
+
+            tabpanels.forEach(tabpanel => {
+                // https://stackoverflow.com/questions/3623110/how-can-i-get-an-elements-id-value-with-javascript
+
+                if(anchor.hash == '#' + tabpanel.id) {
+                    tabpanels.forEach(tabpanelCopy => {tabpanelCopy.style.display = 'none';})
+                    tabpanel.style.display = 'block';
+                }  else {
+                    tabpanel.style.display = 'none';
+                }
+            })
+        } else {
+            anchor.classList.remove('open');
+        }
+    })
+})
 
 // Used this a bit https://www.w3schools.com/howto/howto_js_tabs.asp
-
-// If user clicks on whatever
-listElements[0].addEventListener("click", (event) => {
-    // If the first list item is clicked
-    console.log("clicked the first item");
-});
-listElements[1].addEventListener("click", (event) => {
-    // If the first list item is clicked
-    console.log("clicked the second item");
-});
-listElements[2].addEventListener("click", (event) => {
-    // If the first list item is clicked
-    console.log("clicked the third item");
-});
