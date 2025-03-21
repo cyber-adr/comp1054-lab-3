b29be1c8cbf7a5ea3ff1031360e562c4027925e0
KThach <sthac341@yahoo.ca>
2025-03-21 18:44:13 -0400

    Commiting stable point before making major changes in CSS. Changed the CSS to show the first tab by default.

--- js/lab3.js
+++ js/lab3.js
@@ -1,29 +1,33 @@
 // Grab the List Elements and the Div elements separately
-const anchorElements = document.querySelectorAll('li>a'); // targets anchor tags inside tabs
+const anchorElements = document.querySelectorAll("li>a"); // targets anchor tags inside tabs
 
-const tabpanels = document.querySelectorAll('[role=\"tabpanel\"]'); // targets divs with role="tabpanel" attributes
+const tabpanels = document.querySelectorAll('[role="tabpanel"]'); // targets divs with role="tabpanel" attributes
 
-anchorElements.forEach(anchor => {
-    anchor.addEventListener('click', event => {
+anchorElements.forEach((anchor) => {
+    anchor.addEventListener("click", (event) => {
         // https://www.geeksforgeeks.org/how-to-check-the-given-element-has-the-specified-class-in-javascript/
-        if (!anchor.classList.contains('open')) {
-            anchorElements.forEach(anchorCopy => {anchorCopy.classList.remove('open')}) // Remove class="open" for all list
-            anchor.classList.add('open'); // Then add the class="open" to the list of the current iteration of the outer loop
+        if (!anchor.classList.contains("open")) {
+            anchorElements.forEach((anchorCopy) => {
+                anchorCopy.classList.remove("open");
+            }); // Remove class="open" for all list
+            anchor.classList.add("open"); // Then add the class="open" to the list of the current iteration of the outer loop
 
-            tabpanels.forEach(tabpanel => {
+            tabpanels.forEach((tabpanel) => {
                 // https://stackoverflow.com/questions/3623110/how-can-i-get-an-elements-id-value-with-javascript
 
-                if(anchor.hash == '#' + tabpanel.id) {
-                    tabpanels.forEach(tabpanelCopy => {tabpanelCopy.style.display = 'none';})
-                    tabpanel.style.display = 'block';
-                }  else {
-                    tabpanel.style.display = 'none';
+                if (anchor.hash == "#" + tabpanel.id) {
+                    tabpanels.forEach((tabpanelCopy) => {
+                        tabpanelCopy.style.display = "none";
+                    });
+                    tabpanel.style.display = "block";
+                } else {
+                    tabpanel.style.display = "none";
                 }
-            })
+            });
         } else {
-            anchor.classList.remove('open');
+            anchor.classList.remove("open");
         }
-    })
-})
+    });
+});
 
 // Used this a bit https://www.w3schools.com/howto/howto_js_tabs.asp
