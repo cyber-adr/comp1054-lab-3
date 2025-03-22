3799d61b78a2b32a38d87cd37aef7ddc7b6e11c2
KThach <sthac341@yahoo.ca>
2025-03-21 20:12:43 -0400

    Commented JS, revamped the CSS to make the tabs work together and look nice.

--- js/lab3.js
+++ js/lab3.js
@@ -6,25 +6,40 @@ const tabpanels = document.querySelectorAll('[role="tabpanel"]'); // targets div
 anchorElements.forEach((anchor) => {
     anchor.addEventListener("click", (event) => {
         // https://www.geeksforgeeks.org/how-to-check-the-given-element-has-the-specified-class-in-javascript/
+        // Check if tab is open already
         if (!anchor.classList.contains("open")) {
-            anchorElements.forEach((anchorCopy) => {
-                anchorCopy.classList.remove("open");
-            }); // Remove class="open" for all list
-            anchor.classList.add("open"); // Then add the class="open" to the list of the current iteration of the outer loop
 
+            // Remove class="open" for all list
+            anchorElements.forEach((anchorCopy) => {
+
+                anchorCopy.classList.remove("open");
+
+            }); 
+
+            // Then add the class="open" to the list of the current iteration of the outer loop
+            anchor.classList.add("open"); 
+
+            // Loop through panels to find match
             tabpanels.forEach((tabpanel) => {
                 // https://stackoverflow.com/questions/3623110/how-can-i-get-an-elements-id-value-with-javascript
 
                 if (anchor.hash == "#" + tabpanel.id) {
+
+                    // Hide all panels first
                     tabpanels.forEach((tabpanelCopy) => {
                         tabpanelCopy.style.display = "none";
                     });
+
+                    // Show the matching panel
                     tabpanel.style.display = "block";
+
                 } else {
+                    // Keep hidden otherwise
                     tabpanel.style.display = "none";
                 }
             });
         } else {
+            // Remove "open" class if tab was already open
             anchor.classList.remove("open");
         }
     });
